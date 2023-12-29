e93369dc236af5addd100b616bdd91b1e3e3436b
Sami chioukh <chioukhsami7@gmail.com>
2023-12-29 12:02:14 +0100

    j'ai enlever la fonction showStartWindow

--- src/App.java
+++ src/App.java
@@ -1,10 +1,6 @@
 import java.awt.Toolkit;
 import javax.swing.JFrame;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-
 public class App {
 
     private static JFrame fenetre;
@@ -21,7 +17,6 @@ public class App {
         fenetre.setVisible(true);
 
         showMenu();
-       //showStartWindow();
     }
 
     public static void repaint() {
@@ -40,30 +35,6 @@ public class App {
     public static void showGame(Game g) {
         fenetre.getContentPane().removeAll();
         fenetre.getContentPane().add(new GameScene(g));
-        fenetre.revalidate();
-        fenetre.repaint();
-        Toolkit.getDefaultToolkit().sync();
-    }
-
-    public static void showStartWindow() {
-        fenetre.getContentPane().removeAll();
-        
-        // Créez un bouton "Jouer"
-        JButton jouerButton = new JButton("Jouer");
-        jouerButton.setBounds(400, 250, 100, 20);
-        
-        // Ajoutez un gestionnaire d'événements pour le bouton "Jouer"
-        jouerButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                showMenu(); // Redirige vers le menu principal (MenuScene)
-               
-            }
-        });
-        
-        // Ajoutez le bouton à la fenêtre
-        fenetre.getContentPane().setLayout(null);
-        fenetre.getContentPane().add(jouerButton);
-        
         fenetre.revalidate();
         fenetre.repaint();
         Toolkit.getDefaultToolkit().sync();
